2312d71f39d7e44628a0f59c9c6f5013bdd12265
GeorgesWK <georgeswilliamkattar@gmail.com>
2020-02-27 16:28:02 -0500

    Setting up CNN 1

--- 2_OwnCNN.py
+++ 2_OwnCNN.py
@@ -7,22 +7,28 @@ import torchvision.transforms as transforms
 import skorch
 import sklearn
 import numpy as np
-
+import os
 import matplotlib
 
 # Load and Transform
-dataset_dir = "../car_train/"
+path = os.getcwd()
+print(path)
 
+dataset_dir = "cars_train"
+
+print(dataset_dir)
 train_tranform = transforms.Compose([transforms.Resize((400, 400)),
                                  transforms.RandomHorizontalFlip(),
                                  transforms.RandomRotation(15),
                                  transforms.ToTensor(),
                                  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-# test = transforms.Compose([transforms.Resize((400, 400)),
-#                                 transforms.ToTensor(),
-#                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-#
-# dataset = torchvision.datasets.ImageFolder(root=dataset_dir+"train", transform = train_tfms)
+
+test_tranform = transforms.Compose([transforms.Resize((400, 400)),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+dataset = torchvision.datasets.ImageFolder(root=dataset_dir, transform=train_tranform)
+
 # trainloader = torch.utils.data.DataLoader(dataset, batch_size = 32, shuffle=True, num_workers = 2)
 #
 # dataset2 = torchvision.datasets.ImageFolder(root=dataset_dir+"test", transform = test_tfms)
