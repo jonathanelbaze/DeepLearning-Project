012c0df791a00354561851bf06e52212cc193457
GeorgesWK <georgeswilliamkattar@gmail.com>
2020-02-27 16:12:46 -0500

    Changed -1 to None for pd.set_option

--- 1_ImportExplo.py
+++ 1_ImportExplo.py
@@ -25,7 +25,7 @@ import matplotlib.pyplot as plt
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_colwidth', None)
 
 
 ########################################           Import Data              ###########################################
@@ -43,50 +43,51 @@ names = pd.read_csv("names.csv", header=None)
 print(names.shape)
 
 
-########################################             Exploration            ###########################################
-print(train.head())
+# ########################################             Exploration            ###########################################
+# print(train.head())
+#
+# # Train - set column names
+# train.columns = ['Image', 'Box 1', 'Box 2', 'Box 3', 'Box 4', 'Y']
+# print(train.head())
+#
+# # Names - Extract Maker
+# names.columns = ['Key']
+# names['Maker'] = names['Key'].str.split(';').str[0]
+#
+#
+# # names['Model']
+# Model = names['Key'].str.split(';').str[1].str.split().str[0:-1]
+#
+# ModelList = []
+# for e in Model:
+#     ModelList.append(''.join(str(a)+' ' for a in e))
+#
+#
+# names['Model'] = ModelList
+#
+# # print(ModelList)
+#
+#
+#
+# # Names - Extract Year
+# names['Year'] = names['Key'].str.split().str[-1]
+# print(names['Year'])
+# # Names - Extract Model
+# info=names['Key']
+# info = info.to_frame()
+# info.columns = ['Year']
+#
+# Models = set(ModelList)
+# print(Models)
+#
+# Makers = set(names['Maker'])
+#
+# MakersDict = {}
+# for idx, car in names['Maker'].iteritems():
+#     print(idx, car)
+#     # if car in MakersDict.keys():
+#     # MakersDict[car].add(idx)
+#
+# print(MakersDict)
 
-# Train - set column names
-train.columns = ['Image', 'Box 1', 'Box 2', 'Box 3', 'Box 4', 'Y']
-print(train.head())
-
-# Names - Extract Maker
-names.columns = ['Key']
-names['Maker'] = names['Key'].str.split(';').str[0]
-
-
-# names['Model']
-Model = names['Key'].str.split(';').str[1].str.split().str[0:-1]
-
-ModelList = []
-for e in Model:
-    ModelList.append(''.join(str(a)+' ' for a in e))
-
-
-names['Model'] = ModelList
-
-# print(ModelList)
-
-
-
-# Names - Extract Year
-names['Year'] = names['Key'].str.split().str[-1]
-print(names['Year'])
-# Names - Extract Model
-info=names['Key']
-info = info.to_frame()
-info.columns = ['Year']
-
-Models = set(ModelList)
-print(Models)
-
-Makers = set(names['Maker'])
-
-MakersDict = {}
-for idx, car in names['Maker'].iteritems():
-    print(idx, car)
-    # if car in MakersDict.keys():
-    # MakersDict[car].add(idx)
-
-print(MakersDict)
 
